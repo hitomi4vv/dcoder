f08066f268fdb70f4128509dbcbcd40bd8d4e4c5
Hitomi Suzuki <hitomi4vv@outlook.jp>
2017-02-18 08:01:17 +0900

    Add any source code files of Printing number using loop

---
+++ easy/Printing number using loop.cs
@@ -0,0 +1,9 @@
+namespace Dcoder {
+  public class Program {
+    public static void Main() {
+      for(int i = 0; i < 10; i++) {
+        System.Console.WriteLine(i+1);
+      }
+    }
+  }
+}
---
+++ easy/Printing number using loop.java
@@ -0,0 +1,7 @@
+class Dcoder {
+  public static void main(String args[]) {
+    for(int i = 0; i < 10; i++) {
+      System.out.println(i+1);
+    }
+  }
+}
